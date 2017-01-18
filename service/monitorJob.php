36d09fc9751beed3e95c8e6b1499573bf535547f
Frank Phillips <frank.phillips@inboxfirst.com>
2017-01-18 18:36:17 -0500

    typo correction on variable

--- service/monitorJob.php
+++ service/monitorJob.php
@@ -42,7 +42,7 @@ if( (empty(Utilities::$domainBlacklists)===true) && (empty(Utilities::$ipBlackli
 
 //update monitor
 $preResult = Utilities::checkBlacklists($monitor['ipDomain']);
-print_r($preResult[0], false);
+print_r($preResult, false);
 if (!$preResult[0]) {
 	print_r($preResult[2], false);
 	$senderScore = floatval($preResult[2]);
