d8aacb0f89f254641d9692b4c51f0eb8713e89e2
Frank Phillips <frank.phillips@inboxfirst.com>
2017-01-23 10:21:21 -0500

    testing SB changes

--- classes/Utilities.class.php
+++ classes/Utilities.class.php
@@ -56,7 +56,7 @@ class Utilities {
 		if(_IpAddresses::isIPAddress($domainOrIp)){
 			foreach(self::$ipBlacklists as $server){
 				$r = self::ipCheck($domainOrIp, $server);
-				
+				echo($r);
 				if($r!='') {
 					if(is_numeric($r) && strlen($r)<=12) {
 						$senderbaseScore = $r;
