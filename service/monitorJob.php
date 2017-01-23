2681fbc9bfcc2383bde218d3612f5ee6e1787cfe
Frank Phillips <frank.phillips@inboxfirst.com>
2017-01-23 10:57:37 -0500

    testing SB changes

--- service/monitorJob.php
+++ service/monitorJob.php
@@ -42,7 +42,7 @@ if( (empty(Utilities::$domainBlacklists)===true) && (empty(Utilities::$ipBlackli
 
 //update monitor
 $preResult = Utilities::checkBlacklists($monitor['ipDomain']);
-print_r($preResult, false);
+// print_r($preResult, false);
 if (!empty($preResult[0]) && $preResultp[0][2] != 0) {
 	 print_r($preResult[0][2], false);
 	$senderScore = floatval($preResult[0][2]);
