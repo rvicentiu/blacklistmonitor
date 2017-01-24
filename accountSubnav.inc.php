4f516c5d147a12b6299a184bc71bec5dbcccd93b
Frank Phillips <frank.phillips@inboxfirst.com>
2017-01-24 10:09:23 -0500

    Move profile in nav.

--- accountSubnav.inc.php
+++ accountSubnav.inc.php
@@ -1,8 +1,9 @@
 <div id="accountSubnav">
 	<ul class="nav nav-tabs">
-		<li <?php if($pageName=='account.php') echo('class="active"');?>><a href="account.php">Profile</a></li>
+		
 		<li <?php if($pageName=='monitorGroup.php') echo('class="active"');?>><a href="monitorGroup.php">Monitor Groups</a></li>
 		<li <?php if($pageName=='blockLists.php') echo('class="active"');?>><a href="blockLists.php">Block Lists</a></li>
 		<li <?php if($pageName=='apiDocumentation.php') echo('class="active"');?>><a href="apiDocumentation.php">API Documentation</a></li>
+		<li <?php if($pageName=='account.php') echo('class="active"');?>><a href="account.php">Profile</a></li>
 	</ul>
 </div>
